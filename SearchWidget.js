3de82d3d74085388cd89f43a63a01a0b9ceca97a
Shaii Ong <monsteronfire@gmail.com>
2017-07-31 23:24:31 +0800

    Add skeleton of SearchWidget library

---
+++ SearchWidget.js
@@ -0,0 +1,21 @@
+(function(global, $) {
+
+  var SearchWidget = function(destination, startDate, endDate, partnerName) {
+    return SearchWidget.init(destination, startDate, endDate, partnerName);
+  };
+
+  SearchWidget.prototype = {};
+
+  SearchWidget.init = function(destination, startDate, endDate, partnerName) {
+    var self = this;
+    self.destination = destination || '';
+    self.startDate = startDate || '';
+    self.endDate = endDate || '';
+    self.partnerName = partnerName || 'default';
+  };
+
+  SearchWidget.init.prototype = SearchWidget.prototype;
+
+  global.SearchWidget = global.SW$ = SearchWidget;
+
+})(window, jQuery);
---
+++ app.js
@@ -0,0 +1,2 @@
+var search = SW$('Kuala Lumpur', '2017-10-10', '2017-11-11', 'airasia');
+console.log(search);
